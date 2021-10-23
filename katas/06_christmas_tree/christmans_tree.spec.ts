c68df3609045cb17df892474897e12ddf2828e7a
Rémi Bruguier <remi@gojob.com>
2021-10-24 00:03:21 +0200

    feat: chritmas tree second iteration

--- katas/06_christmas_tree/christmans_tree.spec.ts
+++ katas/06_christmas_tree/christmans_tree.spec.ts
@@ -2,11 +2,21 @@ import { chrimastTree } from "./christmas_tree";
 
 describe("christmasTree", () => {
   it("renders a three rows tree", () => {
-    const expectedTree = `  
+    const expectedTree = `
   *
  ***
 *****
   |`;
     expect(chrimastTree(3)).toEqual(expectedTree);
+  });
+
+  it("renders a four rows tree", () => {
+    const expectedTree = `
+   *
+  ***
+ *****
+*******
+   |`;
+    expect(chrimastTree(4)).toEqual(expectedTree);
   });
 });
--- katas/06_christmas_tree/christmas_tree.ts
+++ katas/06_christmas_tree/christmas_tree.ts
@@ -1,7 +1,19 @@
 export const chrimastTree = (rowsAmount: number): string => {
-  return `  
-  *
- ***
-*****
-  |`;
+  const arrayOfRowNumbers = Array.from(Array(rowsAmount + 1).keys());
+
+  const leaves = arrayOfRowNumbers
+    .map((rowNumber) => {
+      if (rowNumber === 0) {
+        return "";
+      }
+      const leftSpacing = " ".repeat(
+        arrayOfRowNumbers.length - (rowNumber + 1)
+      );
+      const stars = "*".repeat(rowNumber * 2 - 1);
+      return `${leftSpacing}${stars}`;
+    })
+    .join("\n");
+  const trunk = " ".repeat(arrayOfRowNumbers.length - 2) + "|";
+
+  return `${leaves}\n${trunk}`;
 };
