af3246f42e753d258fe4d0fb34eb3bd249775d1d
Rémi Bruguier <remi@gojob.com>
2021-10-24 00:11:50 +0200

    feat: chritmas tree final iteration

--- katas/06_christmas_tree/christmans_tree.spec.ts
+++ katas/06_christmas_tree/christmans_tree.spec.ts
@@ -19,4 +19,15 @@ describe("christmasTree", () => {
    |`;
     expect(chrimastTree(4)).toEqual(expectedTree);
   });
+
+  it("renders a five rows tree", () => {
+    const expectedTree = `
+    *
+   ***
+  *****
+ *******
+*********
+    |`;
+    expect(chrimastTree(5)).toEqual(expectedTree);
+  });
 });
--- katas/06_christmas_tree/christmas_tree.ts
+++ katas/06_christmas_tree/christmas_tree.ts
@@ -1,19 +1,33 @@
 export const chrimastTree = (rowsAmount: number): string => {
   const arrayOfRowNumbers = Array.from(Array(rowsAmount + 1).keys());
+  const numberOfRows = arrayOfRowNumbers.length;
 
   const leaves = arrayOfRowNumbers
     .map((rowNumber) => {
       if (rowNumber === 0) {
         return "";
       }
-      const leftSpacing = " ".repeat(
-        arrayOfRowNumbers.length - (rowNumber + 1)
-      );
-      const stars = "*".repeat(rowNumber * 2 - 1);
+      const leftSpacing = getCurrentRowLeftSpacing(numberOfRows, rowNumber);
+      const stars = getCurrentRowStars(rowNumber);
       return `${leftSpacing}${stars}`;
     })
     .join("\n");
-  const trunk = " ".repeat(arrayOfRowNumbers.length - 2) + "|";
+
+  const trunk = `${getHalfTreeWidth(numberOfRows)}|`;
 
   return `${leaves}\n${trunk}`;
 };
+
+
+const getCurrentRowLeftSpacing = (numberOfRows: number, rowNumber: number) => {
+  return " ".repeat(numberOfRows - (rowNumber + 1));
+};
+
+const getCurrentRowStars = (rowNumber: number) => {
+  return "*".repeat(rowNumber * 2 - 1);
+};
+
+const getHalfTreeWidth = (numberOfRows: number) => {
+    return " ".repeat(numberOfRows - 2);
+  };
+  
