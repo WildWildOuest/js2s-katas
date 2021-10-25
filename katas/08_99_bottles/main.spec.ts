acfc4d461ec357a97248b46ef7fa6aa297136e27
Rémi Bruguier <remi@gojob.com>
2021-10-25 13:39:00 +0200

    feat: 99 bottles of beer second iteration

--- katas/08_99_bottles/main.spec.ts
+++ katas/08_99_bottles/main.spec.ts
@@ -11,4 +11,10 @@ describe('99 bottles of beer', () => {
       '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
     expect(song.verse(1)).toEqual(expectedVerse);
   });
+
+  it('returns the verse for 2 bottles of beer', () => {
+    const expectedVerse =
+      '2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n';
+    expect(song.verse(2)).toEqual(expectedVerse);
+  });
 });
--- katas/08_99_bottles/main.ts
+++ katas/08_99_bottles/main.ts
@@ -1,5 +1,8 @@
 export default class BeerSong {
-  verse(_: number) {
-    return '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
+  verse(verseNumber: number) {
+    if (verseNumber === 1) {
+      return '1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n';
+    }
+    return `2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n`;
   }
 }
