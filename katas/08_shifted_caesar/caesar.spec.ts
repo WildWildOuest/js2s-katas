a111d033ad13be500e1d9828fba692c66b8a7260
Rémi Bruguier <remi@gojob.com>
2021-10-27 23:36:48 +0200

    shifted caesar final iteration

--- katas/08_shifted_caesar/caesar.spec.ts
+++ katas/08_shifted_caesar/caesar.spec.ts
@@ -18,4 +18,16 @@ describe('shifted caesar', () => {
     const shift = 5;
     expect(shiftedCaesar(rawString, shift)).toEqual('fhj');
   });
+
+  it('should handle alphabet overflow', () => {
+    const rawString = 'xyz';
+    const shift = 2;
+    expect(shiftedCaesar(rawString, shift)).toEqual('zbd');
+  });
+
+  it('should handle alphabet large overflow', () => {
+    const rawString = 'xyz';
+    const shift = 27;
+    expect(shiftedCaesar(rawString, shift)).toEqual('yac');
+  });
 });
--- katas/08_shifted_caesar/caesar.ts
+++ katas/08_shifted_caesar/caesar.ts
@@ -1,10 +1,17 @@
 export const shiftedCaesar = (rawString: string, shift: number) => {
+  const Z_LETTER_CHAR_CODE = 122;
+  const ALPHABET_LENGTH = 26;
   return rawString
     .split('')
-    .map((letter, index) => {
+    .map((_letter, index) => {
       const letterCharCode = rawString.charCodeAt(index);
-      const letterShfit = index + shift;
-      return String.fromCharCode(letterCharCode + letterShfit);
+      const letterShift = index + (shift % ALPHABET_LENGTH);
+      let shiftedLetterCharCode = letterCharCode + letterShift;
+
+      if (shiftedLetterCharCode > Z_LETTER_CHAR_CODE) {
+        shiftedLetterCharCode -= ALPHABET_LENGTH;
+      }
+      return String.fromCharCode(shiftedLetterCharCode);
     })
     .join('');
 };
